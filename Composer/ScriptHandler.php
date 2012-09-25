0e4803790cf987653c7cfe17ed352cca1d77eaa6
dafuer <dafuer@gmail.com>
2012-09-25 13:57:07 +0200

    Fixed composer problems

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -1,23 +1,12 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 
-namespace Sensio\Bundle\DistributionBundle\Composer;
+namespace Dafuer\JpgraphBundle\Composer;
 
-use Symfony\Component\ClassLoader\ClassCollectionLoader;
+/*use Symfony\Component\ClassLoader\ClassCollectionLoader;
 use Symfony\Component\Process\Process;
-use Symfony\Component\Process\PhpExecutableFinder;
+use Symfony\Component\Process\PhpExecutableFinder;*/
 
-/**
- * @author Jordi Boggiano <j.boggiano@seld.be>
- */
 class ScriptHandler
 {
     public static function setupJpgraph($event)
