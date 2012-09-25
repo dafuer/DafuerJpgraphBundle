5726e7a079fdeeb3ab924b460a613c64152f9396
dafuer <dafuer@gmail.com>
2012-09-25 12:57:11 +0200

    Improving composer adding commands

---
+++ Composer/ScriptHandler.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sensio\Bundle\DistributionBundle\Composer;
+
+use Symfony\Component\ClassLoader\ClassCollectionLoader;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\PhpExecutableFinder;
+
+/**
+ * @author Jordi Boggiano <j.boggiano@seld.be>
+ */
+class ScriptHandler
+{
+    public static function clearCache($event)
+    {
+        $options = self::getOptions($event);
+        $appDir = $options['symfony-app-dir'];
+
+        if (!is_dir($appDir)) {
+            echo 'The symfony-app-dir ('.$appDir.') specified in composer.json was not found in '.getcwd().', can not clear the cache.'.PHP_EOL;
+
+            return;
+        }
+
+        static::executeCommand($event, $appDir, 'dafuerjpgraph:fixantialiaserror');
+        static::executeCommand($event, $appDir, 'dafuerjpgraph:installaconstants');
+    }
+
+   
+}
